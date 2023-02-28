6e0d9239f190737820c631e5d6813e8a1c54b724
Evgeniy Ermakov <eugene.ermakoff@gmail.com>
2023-02-28 21:09:55 +0300

    Модели созданы

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+
+from django.core.validators import (RegexValidator, MaxValueValidator,
+                                    MinValueValidator)
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        validators=[RegexValidator(regex=r'^[-a-zA-Z0-9_]+$',
+                                   message='invalid slug.')]
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        validators=[RegexValidator(regex=r'^[-a-zA-Z0-9_]+$',
+                                   message='invalid slug.')]
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.TextField(verbose_name='Название')
+    year = models.IntegerField(
+        verbose_name='Год произведения',
+        validators=[MaxValueValidator(datetime.now().year),
+                    MinValueValidator(0)]
+    )
+    description = models.TextField(blank=True, null=True)
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        blank=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
