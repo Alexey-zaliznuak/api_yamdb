6b63e2b1ce3b761421d3855ef187307b58a32bf0
Evgeniy Ermakov <eugene.ermakoff@gmail.com>
2023-02-28 21:19:30 +0300

    админка

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Title, Category, Genre
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category', 'genre', 'name', 'description', 'year',)
+    search_fields = ('name', 'year')
+    list_filter = ('year',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category)
+admin.site.register(Genre)
