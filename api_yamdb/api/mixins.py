2ba6b9c34aa6a8b2c266e8e3abbe0f73e47c7c6e
Evgeniy Ermakov <eugene.ermakoff@gmail.com>
2023-03-06 20:19:06 +0300

    filters

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,14 +1,12 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, filters
 from rest_framework.pagination import PageNumberPagination
-
-from users.permissions import ermtest
 
 
 class ListCreateDestroyViewSet(mixins.ListModelMixin,
-                              mixins.CreateModelMixin,
-                              mixins.DestroyModelMixin,
-                              viewsets.GenericViewSet):
+                               mixins.CreateModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
     pagination_class = PageNumberPagination
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
-    # permission_classes = (ermtest,)
